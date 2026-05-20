655d929307173b2ba94f791f461b591e740c9cb1
mohangannamani <142310043+mohangannamani@users.noreply.github.com>
2026-05-20 15:52:43 +0530

    Delete Changes Over Time Analysis.sql

--- Changes Over Time Analysis.sql
+++
@@ -1,9 +0,0 @@
-select
-datetrunc(month,order_date) as date_trunc,
-sum(sales_amount) as total_sales,
-count(distinct customer_key) as toal_customers,
-sum(quantity) as total_quantity
-from gold.fact_sales
-where order_date is not null
-group by datetrunc(month,order_date)
-order by datetrunc(month,order_date)
